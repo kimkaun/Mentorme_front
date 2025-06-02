ee7541ae68a9487d267ee27845bb8c7968849f03
82107 <82107@>
2025-06-02 11:11:26 +0900

    게시판, 회사추천, 마이페이지상세 구현완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { createRoot } from 'react-dom/client';
 //import App from './App.jsx';
 //import LoginPage from './Login_SignUp/components/LoginPage';
-//import SignupPage from './Login_SignUp/components/SignUpPage';
+import SignupPage from './Login_SignUp/components/SignUpPage';
 //import BoardInput from './Board/components/BoardInput';
 //import Board from './Board/components/Board';
 //import Recommend from './Recommend/components/Recommend';
-import Mypage from './Mypage/components/Mypage';
+//import Mypage from './Mypage/components/Mypage';
 
 //createRoot(document.getElementById('root')).render(<App/>);
 // createRoot(document.getElementById('root')).render(<LoginPage/>);
-//createRoot(document.getElementById('root')).render(<SignupPage/>);
+createRoot(document.getElementById('root')).render(<SignupPage/>);
 //createRoot(document.getElementById('root')).render(<BoardInput/>);
 //createRoot(document.getElementById('root')).render(<Board/>);
 //createRoot(document.getElementById('root')).render(<Recommend/>);
-createRoot(document.getElementById('root')).render(<Mypage/>);
+//createRoot(document.getElementById('root')).render(<Mypage/>);
+
